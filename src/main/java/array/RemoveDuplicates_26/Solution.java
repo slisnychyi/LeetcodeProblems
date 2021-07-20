325335238798cb43a1400d3c3c40021b1500c571
Sergii Lisnychyi <sergiilisnychyi@slisnychyis-macbook.local>
2021-07-20 18:15:29 +0300

    20.07

--- src/main/java/array/RemoveDuplicates_26/Solution.java
+++ src/main/java/array/RemoveDuplicates_26/Solution.java
@@ -1,20 +1,25 @@
 package array.RemoveDuplicates_26;
 
+import java.util.Arrays;
+
 public class Solution {
-    public int removeDuplicates(int[] nums) {
-        if(nums.length == 0) return nums.length;
 
-        int result = 1;
-        int toInsert = 1;
-        int prev = nums[0];
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 3, 4, 4, 4, 5};
+        System.out.println(removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
+    }
 
-        for(int i = 1; i < nums.length; i++){
-            if(prev != nums[i]){
-                nums[toInsert++] = nums[i];
-                prev = nums[i];
-                result++;
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return nums.length;
+
+        int prev = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[prev] != nums[i]) {
+                nums[++prev] = nums[i];
             }
         }
-        return result;
+        return prev + 1;
     }
 }
